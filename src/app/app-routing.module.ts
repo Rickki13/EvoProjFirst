f50090d7071ba24b78716bdfe1cf1de1dfb1c336
droschihin <dima2016123@yandex.ru>
2023-09-25 01:23:35 +0300

    Task 1.7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {InlineComponent} from "./inline/inline.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'open-graph',
+    component: InlineComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'EvoProjFirst';
+  constructor(private meta: Meta)
+  {
+    this.meta.addTag({ property: 'og:desc', content: 'root_desc'});
+  }
+  ngOnInit() {
+  }
 }
