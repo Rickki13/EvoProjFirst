c2de4abe91ab7763057631012f81e8016baff303
droschihin <dima2016123@yandex.ru>
2023-09-25 19:57:04 +0300

    Task 1.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import {Notify} from "notiflix/build/notiflix-notify-aio";
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'EvoProjFirst';
+
+  notiflix_alert(type: string) {
+    switch (type) {
+      case 'success':
+        Notify.success('Success Alert');
+        break;
+      case 'warning':
+        Notify.warning('Warning Alert');
+        break;
+      case 'error':
+        Notify.failure('Error Alert');
+        break;
+      case 'info':
+        Notify.info('Info Alert');
+        break;
+    }
+  }
+
+
+
+  HelloBtn(){
+    console.log("Hello!");
+  }
+  ByeBtn(){
+    console.log("Bye!");
+  }
 }
