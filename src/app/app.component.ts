988314353276f606e7bdc1d8fbe16ef27cc62368
droschihin <dima2016123@yandex.ru>
2023-09-29 18:28:12 +0300

    Task 2.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface BookInt{
+  title: string | null;
+  author: string | null;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EvoProjFirst';
+  books: BookInt[] = [
+    {
+      title: 'Начало после конца',
+      author: 'Брэндон Ли'
+    },
+    {
+      title: 'Война и мир',
+      author: 'Лев Толстой'
+    },
+    {
+      title: 'Мастер и Маргарита',
+      author: 'Михаил Булгаков'
+    }
+  ]
+
+  newBook = {
+    title: null,
+    author: null,
+  };
+
+  createBook() {
+    this.books.push({
+      title: this.newBook.title,
+      author: this.newBook.author,
+    });
+  }
 }
