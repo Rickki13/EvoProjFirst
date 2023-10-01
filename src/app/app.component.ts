b39acda880952f4f2d82e83f7daeb5e45534f598
droschihin <dima2016123@yandex.ru>
2023-10-01 19:37:03 +0300

    Task 2.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {interval, map, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EvoProjFirst';
+
+  constructor() {}
+
+  intervalSubscrip$!: Subscription;
+  intervalSubscripRand$!: Subscription;
+  numArray : number[] = [];
+  numArrayRand : string[] = [];
+
+  enableIntervalOperator(){
+    const intervalStream$ = interval(2000)
+    this.intervalSubscrip$ = intervalStream$.subscribe((value) => {
+        this.numArray.push(value);
+    })
+    this.intervalSubscripRand$ = intervalStream$.pipe(
+      map(value => `Random Value: ${Math.floor(getRandom(0,1000)) }`)
+    ).subscribe((value) => {
+      this.numArrayRand.push(value);
+    })
+  }
+
+  disableIntervalOperator(){
+    this.intervalSubscrip$.unsubscribe();
+  }
+
+  disableIntervalOperatorRand(){
+    this.intervalSubscripRand$.unsubscribe();
+  }
+}
+
+function getRandom(min: number, max: number){
+  return Math.random() * (max - min) + min;
 }
