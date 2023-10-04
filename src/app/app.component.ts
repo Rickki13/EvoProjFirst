478554ef700503fdf6b4dd75c2cc6d5cea5b3403
droschihin <dima2016123@yandex.ru>
2023-10-04 20:55:33 +0300

    Task 2.3 Button fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,25 +15,35 @@ export class AppComponent {
   intervalSubscripRand$!: Subscription;
   numArray : number[] = [];
   numArrayRand : string[] = [];
+  enableNum: boolean = false;
+  enableNumRand: boolean = false;
 
   enableIntervalOperator(){
     const intervalStream$ = interval(2000)
-    this.intervalSubscrip$ = intervalStream$.subscribe((value) => {
+    if (!this.enableNum){
+      this.intervalSubscrip$ = intervalStream$.subscribe((value) => {
         this.numArray.push(value);
-    })
-    this.intervalSubscripRand$ = intervalStream$.pipe(
-      map(value => `Random Value: ${Math.floor(getRandom(0,1000)) }`)
-    ).subscribe((value) => {
-      this.numArrayRand.push(value);
-    })
+        this.enableNum = true;
+      })
+    }
+    if(!this.enableNumRand){
+      this.intervalSubscripRand$ = intervalStream$.pipe(
+        map(value => `Random Value: ${Math.floor(getRandom(0,1000)) }`)
+      ).subscribe((value) => {
+        this.numArrayRand.push(value);
+        this.enableNumRand = true;
+      })
+    }
   }
 
   disableIntervalOperator(){
     this.intervalSubscrip$.unsubscribe();
+    this.enableNum = false;
   }
 
   disableIntervalOperatorRand(){
     this.intervalSubscripRand$.unsubscribe();
+    this.enableNumRand = false;
   }
 }
 
