5aa7267be3bb55eea58d2d625afa9d7400f31adb
droschihin <dima2016123@yandex.ru>
2023-10-08 16:28:06 +0300

    Task 2.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, ComponentRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {DynTableComponent} from "./dyn-table/dyn-table.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'EvoProjFirst';
+
+  @ViewChild('dynTable', { read: ViewContainerRef })
+  private viewRef!: ViewContainerRef;
+  private componentRef!: ComponentRef<DynTableComponent>;
+
+  addComponent() {
+    this.viewRef.clear();
+    this.componentRef = this.viewRef.createComponent(DynTableComponent);
+  }
+
+  deleteComponent() {
+    this.viewRef.clear();
+  }
 }
---
+++ src/app/dyn-table/dyn-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dyn-table',
+  templateUrl: './dyn-table.component.html',
+  styleUrls: ['./dyn-table.component.css']
+})
+export class DynTableComponent {
+
+}
